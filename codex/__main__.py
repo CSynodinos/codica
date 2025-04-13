de2372d15b9cbbff11960ff662f81ba63179dcd4
CSynodinos <chris_sinodinos@outlook.com>
2025-04-13 13:23:23 +0100

    -

--- codex/__main__.py
+++ codex/__main__.py
@@ -9,41 +9,32 @@ def Codex_App() -> codex.Codex:
     Create the main parser and subparsers for the CLI application
     """
 
-    #* Create the main parser & subparsers
     app = ArgumentParser(prog = "codex", description = "Codex CLI Application")
     subparsers: codex.Subparser = app.add_subparsers(title = "Commands", dest = "command", required = True)
 
-    #* Create 'create' subcommand and add flags
     create_subcmd = codex.Create_Subcommand(subparsers, "create", help = "Create a new item")
     codex.Add_Argument(create_subcmd, "--record", required = True, help = "Record identifier")
     codex.Add_Argument(create_subcmd, "--set", required = True, help = "Name of the item")
 
-    #* Update subcommand and add flags
     update_subcmd = codex.Create_Subcommand(subparsers, "update", help = "Update an item")
     
 
-    #* Delete subcommand and add flags
     delete_subcmd = codex.Create_Subcommand(subparsers, "delete", help = "Delete an item")
     
 
 
-    #* Deploy subcommand and add flags
     deploy_subcmd = codex.Create_Subcommand(subparsers, "deploy", help = "Deploy your project")
     
 
-    #* Pack subcommand and add flags
     pack_subcmd = codex.Create_Subcommand(subparsers, "pack", help = "Pack resources")
     
 
-    #* Unpack subcommand and add flags
     unpack_subcmd = codex.Create_Subcommand(subparsers, "unpack", help = "Unpack resources")
     
 
-    #* Build subcommand and add flags
     build_subcmd = codex.Create_Subcommand(subparsers, "build", help = "Build your project")
     
 
-    #* Bootstrap the subcommands with behaviours
     Behaviours_and_Subcommands: list[tuple[codex.Behaviour, codex.Subcommand]] = [
         (codex.create, create_subcmd),
         (codex.update, update_subcmd),
@@ -54,7 +45,6 @@ def Codex_App() -> codex.Codex:
         (codex.build, build_subcmd)
         ]
 
-    #* Set the behaviours for each subcommand
     for behaviour, subcommand in Behaviours_and_Subcommands:
         codex.Set_Behaviour(behaviour, subcommand)
 
@@ -63,8 +53,10 @@ def Codex_App() -> codex.Codex:
 
 def main() -> None:
     app = Codex_App()
-    cli_params: dict[str, Any] = vars(app.parse_args())
-    return codex.Run(codex.CodexParams(**cli_params))
+    params: dict[str, Any] = vars(app.parse_args())
+    return codex.Run(
+        codex.CodexParams(**params)
+        )
 
 
 if __name__ == '__main__':
