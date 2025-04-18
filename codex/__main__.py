a6a949b5ed3d2e373e3862e9c886a19e50bc23c5
CSynodinos <chris_sinodinos@outlook.com>
2025-04-18 23:06:38 +0100

    more args

--- codex/__main__.py
+++ codex/__main__.py
@@ -12,29 +12,30 @@ def Codex_App() -> codex.Codex:
     app = ArgumentParser(prog = "codex", description = "Codex CLI Application")
     subparsers: codex.Subparser = app.add_subparsers(title = "Commands", dest = "command", required = True)
 
-    create_subcmd = codex.Create_Subcommand(subparsers, "create", help = "Create a new item")
-    codex.Add_Argument(create_subcmd, "directory", nargs = "*", help = "Arguments to pass to the command")
-    codex.Add_Argument(create_subcmd, "--outdir", "-o", help = "Output directory", default = "docs")
+    create_subcmd = codex.Create_Subcommand(subparsers, codex.Commands.CREATE, help = "Create a new codex project")
+    codex.Add_Argument(create_subcmd, "directory", nargs = "*", help = "Directory to create docs for")
+    codex.Add_Argument(create_subcmd, "--outdir", "-o", help = "Output directory, default docs", default = "docs")
     
     
-    update_subcmd = codex.Create_Subcommand(subparsers, "update", help = "Update an item")
-    
-
-    delete_subcmd = codex.Create_Subcommand(subparsers, "delete", help = "Delete an item")
+    update_subcmd = codex.Create_Subcommand(subparsers, codex.Commands.UPDATE, help = "Update an item")
+    codex.Add_Argument(update_subcmd, "directory", nargs = "*", help = "Directory to update", default = ("docs",))
     
 
+    delete_subcmd = codex.Create_Subcommand(subparsers, codex.Commands.DELETE, help = "Delete an item")
+    codex.Add_Argument(delete_subcmd, "directory", nargs = "*", help = "Directory to delete", default = ("docs",))
 
-    deploy_subcmd = codex.Create_Subcommand(subparsers, "deploy", help = "Deploy your project")
-    codex.Add_Argument(deploy_subcmd, "directory", nargs = "*", help = "Arguments to pass to the command", default = ["docs",])
+
+    deploy_subcmd = codex.Create_Subcommand(subparsers, codex.Commands.DEPLOY, help = "Deploy your project")
+    codex.Add_Argument(deploy_subcmd, "directory", nargs = "*", help = "Arguments to pass to the command", default = ("docs",))
     codex.Add_Argument(deploy_subcmd, "--port", "-p", help = "Port to use", default = "8000")
 
-    pack_subcmd = codex.Create_Subcommand(subparsers, "pack", help = "Pack resources")
+    pack_subcmd = codex.Create_Subcommand(subparsers, codex.Commands.PACK, help = "Pack resources")
     
 
-    unpack_subcmd = codex.Create_Subcommand(subparsers, "unpack", help = "Unpack resources")
+    unpack_subcmd = codex.Create_Subcommand(subparsers, codex.Commands.UNPACK, help = "Unpack resources")
     
 
-    build_subcmd = codex.Create_Subcommand(subparsers, "build", help = "Build your project")
+    build_subcmd = codex.Create_Subcommand(subparsers, codex.Commands.BUILD, help = "Build your project")
     
 
     Behaviours_and_Subcommands: list[tuple[codex.Behaviour, codex.Subcommand]] = [
